e8844169de2abb2670d5ed47fe46edf66347af2d
MechanicalPickles25036 <mechanicalpickles.25036@gmail.com>
2026-03-26 14:41:03 +0200

    Refactor intake motor control methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newcode/SubSystem/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newcode/SubSystem/intake.java
@@ -6,26 +6,33 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Gamepad;
 public class intake {
-
+    private DcMotor motor2;
     private DcMotor motor;
 
     private HardwareMap hardwareMap;
 
 
     public void init(HardwareMap hardwareMap){
-        motor = hardwareMap.get(DcMotor.class, "intake_motor");
-
+        motor = hardwareMap.get(DcMotor.class, "intake_motor1");
+        motor.setDirection(DcMotor.Direction.REVERSE);
+//        motor2 = hardwareMap.get(DcMotor.class, "intake_motor2");
+//        motor2.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void start(){
+    public void start_in(){
+        motor.setPower(-1);
+//        motor2.setPower(1);
+    }
+    public void start_out(){
         motor.setPower(1);
+//        motor2.setPower(-0.65);
     }
-
     public void stop(){
         motor.setPower(0);
-
+//        motor2.setPower(0);
     }
+
+
+
+
 }
-
-
-
