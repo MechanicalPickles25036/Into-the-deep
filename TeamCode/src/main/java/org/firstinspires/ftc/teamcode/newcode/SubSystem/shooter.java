9d0083d707c43e6e6c4b456a7514d9544a160491
MechanicalPickles25036 <mechanicalpickles.25036@gmail.com>
2026-03-26 14:41:44 +0200

    Refactor shooter class to support dual motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newcode/SubSystem/shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newcode/SubSystem/shooter.java
@@ -7,28 +7,36 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class shooter {
 
-    private DcMotor motor;
-
+    private DcMotor motor1;
+    private DcMotor motor2;
     private HardwareMap hardwareMap;
 
 
 
     public void init(HardwareMap hardwareMap){
-        motor = hardwareMap.get(DcMotor.class, "shooter_motor");
+        motor1 = hardwareMap.get(DcMotor.class, "shooter_motor1");
+        motor2 = hardwareMap.get(DcMotor.class, "shooter_motor2");
+        motor1.setDirection(DcMotor.Direction.REVERSE);
+        motor2.setDirection(DcMotor.Direction.REVERSE);
+
     }
 
 
 
     public void start(){
-        motor.setPower(1);
+        motor1.setPower(1);
+        motor2.setPower(1);
+
     }
 
     public void stop(){
-        motor.setPower(0);
-
+        motor1.setPower(0);
+        motor2.setPower(0);
     }
 
-
-
-
-}
+    public void reverse()
+    {
+        motor1.setPower(-0.5);
+        motor2.setPower(-0.5);
+    }
+    }
